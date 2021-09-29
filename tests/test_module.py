eb14afed88c6d2bea8e2c7ab8869e4a423738f1e
fy w <1697256461@qq.com>
2021-09-29 21:52:51 +0800

    finish test0_4

--- tests/test_module.py
+++ tests/test_module.py
@@ -146,7 +146,7 @@ class ModuleRun(minitorch.Module):
 
 
 @pytest.mark.task0_4
-@pytest.mark.xfail
+@pytest.mark.xfail # 这里定义了xfail！
 def test_module_fail_forward():
     mod = minitorch.Module()
     mod()
